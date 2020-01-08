30954e95c7e69b76de031f898b8fd49ecadfc80a
MariaPaulaS <mariathecharmix@hotmail.com>
2020-01-08 16:59:52 -0500

    Registro de clientes - sin conectar a la tabla

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -3,52 +3,53 @@
 @section('content')
     <div class="container">
         <div class="col-sm-offset-3 col-sm-6">
-                <div class="panel-title">
-                  <h1>Agregar cliente</h1>
+            <div class="panel-title">
+                <h1>Agregar cliente</h1>
 
-                </div>
-<!-- Bootstrap Boilerplate... -->
+            </div>
+            <!-- Bootstrap Boilerplate... -->
 
-<div class="panel-body">
-    <!-- Display Validation Errors -->
-    @include('common.errors')
+            <div class="panel-body">
+                <!-- Display Validation Errors -->
+            @include('common.errors')
 
-    <!-- New Task Form -->
-    <form action="/index" method="POST" class="form-horizontal">
-        {{ csrf_field() }}
+            <!-- New Task Form -->
+                <form action={{ url('clients') }} method="POST" class="form-horizontal">
+                {{ csrf_field() }}
 
-        <!-- Attributes Names -->
-        <div class="form-group">
-            <label for="client-first-name">Nombre</label>
-                <input type="text" name="name" id="client-first-name" class="form-control">
+                <!-- Attributes Names -->
+                    <div class="form-group">
+                        <label for="first_name">Nombre</label>
+                        <input type="text" name="first_name" id="first_name" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="last_name">Apellido</label>
+                        <input type="text" name="last_name" id="last_name" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="id_card">Identificación</label>
+                        <input type="text" name="id_card" id="id_card" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="number_phone">Telefono</label>
+                        <input type="text" name="number_phone" id="number_phone" class="form-control">
+                    </div>
+
+                    <!-- Add Task Button -->
+                    <div class="form-group">
+
+                        <button type="submit" class="btn btn-black">
+                            <i class="fa fa-plus"></i> Registrar cliente
+
+                        </button>
+
+                    </div>
+                </form>
+            </div>
         </div>
-
-            <div class="form-group">
-                <label for="client-last-name">Apellido</label>
-                <input type="text" name="name" id="client-last-name" class="form-control">
-            </div>
-
-            <div class="form-group">
-                <label for="client-id-card">Identificación</label>
-                <input type="text" name="name" id="client-id-card" class="form-control">
-            </div>
-
-            <div class="form-group">
-                <label for="client-phone">Telefono</label>
-                <input type="text" name="name" id="client-phone" class="form-control">
-            </div>
-
-            <!-- Add Task Button -->
-        <div class="form-group">
-
-                <button type="submit" class="btn btn-black">
-                    <i class="fa fa-plus"></i> Registrar cliente
-
-                </button>
-
-        </div>
-    </form>
-</div>
-
-<!-- TODO: Current Tasks -->
+    </div>
+    <!-- TODO: Current Tasks -->
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/clients', function (){
-
-    return view('clients/create');
-
-});
+Route::resource("/clients", 'ClientsController');
